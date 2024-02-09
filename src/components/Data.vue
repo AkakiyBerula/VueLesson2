81b1dc334afb30d9d61c742732877cd6961c21f1
AkakiyBerula <vlad5dyakun.gmail.com>
2024-02-09 15:38:40 +0200

    Створіть метод, який приймає подію і використовує ії для зміни даних інстанса.

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -4,14 +4,37 @@
             <h1 class="display-4">Personal Data</h1> 
             <p class="lead">{{ firstName }}</p> 
             <p class="lead">{{ lastName }}</p>
+            <div>
+                <p class="lead">Ввести ім'я</p>
+                <input type="text" :value="firstName" @input="firstName = $event.target.value">
+                
+            </div>
+            <div>
+                <p class="lead">Ввести прізвище</p>
+                <input type="text" :value="lastName" @input="lastName = $event.target.value">
+            </div>
             <p class="lead">{{ age }}</p>
+            <div>
+                <p>Змінити вік користувача</p>
+                <button class="btn btn-success" @click="incrementAge">+</button>
+                <button class="btn btn-danger" @click="decrementAge">-</button>
+            </div>
             <p class="lead">{{ height }} cm</p>
             <p class="lead">{{ weight }} kg</p>
         </div>
         <div class="container text-center">
             <h1 class="display-4">Other Data</h1> 
             <p class="lead">{{ friend.name }}</p> 
+            <div>
+                <p class="lead">Ввести ім'я друга</p>
+                <input type="text" :value="friend.name" @input="friend.name = $event.target.value">
+            </div>
             <p class="lead">{{ friend.age }}</p>
+            <div>
+                <p>Змінити вік друга користувача</p>
+                <button class="btn btn-success" @click="incrementFriendAge">+</button>
+                <button class="btn btn-danger" @click="decrementFriendAge">-</button>
+            </div>
             <p class="lead">{{ friend.height }} cm</p>
             <p class="lead">{{ friend.weight }} kg</p>
             <p class="lead">{{ friend.friendsList }}</p>
@@ -26,7 +49,6 @@ export default {
     name: 'Data',
     data() {
         return {
-            count: 0,
             firstName: "John",
             lastName: "Doering",
             age: 29,
@@ -41,7 +63,20 @@ export default {
             }
         }
     },
-    
+    methods: {
+        incrementAge () {
+            this.age++
+        },
+        decrementAge () {
+            this.age--
+        },
+        incrementFriendAge () {
+            this.friend.age++
+        },
+        decrementFriendAge () {
+            this.age--
+        },
+    },
 }
 
 </script>
